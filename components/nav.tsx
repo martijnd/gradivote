37c3dd43d9ab674dd979c246338f668a64e739f9
Martijn Dorsman <martijn.dorsman@gmail.com>
2021-01-23 11:44:18 +0100

    Fresh

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,16 +2,16 @@ import Link from 'next/link'
 
 const links = [
   { href: 'https://github.com/vercel/next.js', label: 'GitHub' },
-  { href: 'https://nextjs.org/docs', label: 'Docs' }
+  { href: 'https://nextjs.org/docs', label: 'Docs' },
 ]
 
-export default function Nav () {
+export default function Nav() {
   return (
     <nav>
       <ul className="flex items-center justify-between p-8">
         <li>
           <Link href="/">
-            <a className="text-blue-500 no-underline dark:text-blue-300">
+            <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300">
               Home
             </a>
           </Link>
